dd5a022ab114c81af45d5be7ef610d58648428a7
Erwin <108355646+Erwin0925@users.noreply.github.com>
2025-01-02 19:28:43 +0800

    Used diff Dtos for diff CRUD method

--- HighSchool-Management-System/DTOs/StudentDtoForGet.cs
+++ HighSchool-Management-System/DTOs/StudentDtoForGet.cs
@@ -1,13 +1,15 @@
 ﻿namespace HighSchool_Management_System.DTOs
 {
-    public class StudentDto
+    public class StudentDtoForGet
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
+        public DateTime Dob { get; set; }
         public string Gender { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public string ClassName { get; set; } // Flattened SchoolClass relationship
+        public int SchoolClassId { get; set; }
+        public string SchoolClassName { get; set; } // Flattened SchoolClass relationship
         public string FormTeacherName { get; set; } // Flattened FormTeacher relationship
     }
 }
---
+++ HighSchool-Management-System/DTOs/StudentDtoForPost.cs
@@ -0,0 +1,12 @@
+﻿namespace HighSchool_Management_System.DTOs
+{
+    public class StudentDtoForPost
+    {
+        public string Name { get; set; }
+        public DateTime Dob { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int SchoolClassId { get; set; }
+    }
+}
