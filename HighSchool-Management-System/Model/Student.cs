8a7b3e4231926d38d6cb1a924a35692469135c7c
Erwin <108355646+Erwin0925@users.noreply.github.com>
2025-01-02 19:28:57 +0800

    Update Student Model Entity

--- HighSchool-Management-System/Model/Student.cs
+++ HighSchool-Management-System/Model/Student.cs
@@ -1,6 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Claims;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HighSchool_Management_System.Model
 {
