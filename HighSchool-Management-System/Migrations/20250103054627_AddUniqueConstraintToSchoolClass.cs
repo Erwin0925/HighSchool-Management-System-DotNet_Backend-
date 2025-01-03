e8ef5f852ffc8f4c4f3b066d531cbbcc2b850b48
Erwin <108355646+Erwin0925@users.noreply.github.com>
2025-01-04 01:16:59 +0800

    migration

---
+++ HighSchool-Management-System/Migrations/20250103054627_AddUniqueConstraintToSchoolClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HighSchool_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueConstraintToSchoolClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_SchoolClasses_ClassName",
+                table: "SchoolClasses",
+                column: "ClassName",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SchoolClasses_ClassName",
+                table: "SchoolClasses");
+        }
+    }
+}
